441e69ecc89d3fa23fdccf284813ac47a5a7755c
Karola Marky <karola@pinyto.de>
2016-01-20 20:10:59 +0100

    Layout for small devices added

--- app/src/main/java/org/secuso/privacyfriendlyyahtzeedicer/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyyahtzeedicer/MainActivity.java
@@ -1,5 +1,6 @@
 package org.secuso.privacyfriendlyyahtzeedicer;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -183,16 +184,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                intent.setClass(this, AboutActivity.class);
+                startActivityForResult(intent, 0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
