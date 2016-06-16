425f2effecd43eb532dffa4de5c6e87e422d5f51
Karola Marky <yonjuni@pinyto.de>
2016-06-17 00:09:17 +0200

    Dice classes added

---
+++ app/src/main/java/org/secuso/privacyfriendlyyahtzeedicer/dice/DiceOne.java
@@ -0,0 +1,39 @@
+package org.secuso.privacyfriendlyyahtzeedicer.dice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by yonjuni on 18.12.15.
+ */
+public class DiceOne extends View{
+
+    Paint paint;
+    View diceButton;
+    float radius;
+
+    public DiceOne(Context context, View diceButton, float dotWidth) {
+        super(context);
+
+        this.diceButton = diceButton;
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.WHITE);
+        this.radius = dotWidth;
+    }
+
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int buttonCenterX = diceButton.getLeft() + diceButton.getWidth()/2;
+        int buttonCenterY = diceButton.getTop() + diceButton.getHeight()/2;
+
+        canvas.drawCircle(buttonCenterX, buttonCenterY, radius, paint);
+    }
+
+}
+
+
