8330ef6a3c104ecae98f72888dcf2491d807fe65
Karola Marky <yonjuni@pinyto.de>
2016-06-16 23:47:52 +0200

    Empty API 23 app

--- app/src/main/java/org/secuso/privacyfriendlyyahtzeedicer/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyyahtzeedicer/MainActivity.java
@@ -1,259 +1,101 @@
 package org.secuso.privacyfriendlyyahtzeedicer;
 
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.util.Log;
-import android.view.Display;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-
-public class MainActivity extends ActionBarActivity {
-
-    private ListView drawerList;
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
-    private String activityTitle;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle(R.string.app_name);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#024265")));
-
-        drawerList = (ListView) findViewById(R.id.navList);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        activityTitle = getTitle().toString();
-
-        addDrawerItems();
-        setupDrawer();
-
-        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[3];
-
-        drawerItem[0] = new ObjectDrawerItem(R.drawable.action_play, getString(R.string.action_play), "");
-        drawerItem[1] = new ObjectDrawerItem(R.drawable.ic_action_help, getString(R.string.action_help), "");
-        drawerItem[2] = new ObjectDrawerItem(R.drawable.ic_action_about, getString(R.string.action_about), "");
-
-        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
-        drawerList.setAdapter(adapter);
-
-        Display display = getWindowManager().getDefaultDisplay();
-
-        final FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        YahtzeeFragment yahtzeeFragment = new YahtzeeFragment(display.getWidth() / 40, Math.round(display.getWidth() / 4));
-        fragmentTransaction.replace(R.id.content_frame, yahtzeeFragment, "YahtzeeFragment");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
-
-    private void addDrawerItems() {
-        String[] mNavigationDrawerItemTitles = {getString(R.string.action_help), getString(R.string.action_about)};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mNavigationDrawerItemTitles);
-        drawerList.setAdapter(adapter);
-
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
-    }
-
-    private void setupDrawer() {
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle(R.string.action_navigation);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
+        });
 
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(activityTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        drawerToggle.setDrawerIndicatorEnabled(true);
-        drawerLayout.setDrawerListener(drawerToggle);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         // Inflate the menu; this adds items to the action bar if it is present.
-        //Remove comment in case menu on the right is needed
-        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-
-    }
-
-    private void selectItem(int position) {
-
-        Fragment fragment = null;
-
-        switch (position) {
-            case 0:
-                Display display = getWindowManager().getDefaultDisplay();
-                fragment = new YahtzeeFragment(display.getWidth() / 40 ,Math.round(display.getWidth() / 4));
-                break;
-            case 1:
-                fragment = new HelpFragment();
-                break;
-            case 2:
-                fragment = new AboutFragment();
-                break;
-            default:
-                break;
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getFragmentManager();
-
-            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).addToBackStack(null).commit();
-
-            drawerList.setItemChecked(position, true);
-            drawerList.setSelection(position);
-            drawerLayout.closeDrawer(drawerList);
-
-        } else {
-            Log.e("MainActivity", "Error in creating fragment");
-        }
-    }
-
-    public class DrawerItemCustomAdapter extends ArrayAdapter<ObjectDrawerItem> {
-
-        Context mContext;
-        int layoutResourceId;
-        ObjectDrawerItem data[] = null;
-
-        public DrawerItemCustomAdapter(Context mContext, int layoutResourceId, ObjectDrawerItem[] data) {
-
-            super(mContext, layoutResourceId, data);
-            this.layoutResourceId = layoutResourceId;
-            this.mContext = mContext;
-            this.data = data;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            View listItem = convertView;
-
-            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
-            listItem = inflater.inflate(layoutResourceId, parent, false);
-
-            ImageView imageViewIcon = (ImageView) listItem.findViewById(R.id.imageViewIcon);
-            TextView textViewName = (TextView) listItem.findViewById(R.id.textViewName);
-            TextView textViewDescription = (TextView) listItem.findViewById(R.id.textViewDescription);
-
-            ObjectDrawerItem folder = data[position];
-
-            imageViewIcon.setImageResource(folder.icon);
-            textViewName.setText(folder.name);
-            textViewDescription.setText(folder.description);
-
-            return listItem;
-        }
-
-    }
-
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public void onBackPressed() {
-        if (getFragmentManager().getBackStackEntryCount() == 1) {
-            finish();
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
         }
 
-        if (getFragmentManager().getBackStackEntryCount() > 0) {
-            getFragmentManager().popBackStack();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    public void tutorialDialogYahtzee() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-
-        alertDialog.setTitle(R.string.tutorial_yahtzee_title);
-
-        alertDialog.setMessage(R.string.tutorial_yahtzee_description);
-
-        alertDialog.setIcon(R.drawable.ic_tutorial);
-
-        alertDialog.setPositiveButton(getString(R.string.confirm_button), new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        });
-
-        alertDialog.setNegativeButton(getString(R.string.help_button), new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-
-                final FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.content_frame, new HelpFragment(), "HelpFragment");
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-                dialog.cancel();
-            }
-        });
-
-        alertDialog.show();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
-
-
-
-
