276ed7e2a863303b4c518e2f246c84950d4d5826
Karola Marky <yonjuni@pinyto.de>
2017-10-16 15:06:52 +0200

    License added to classes

--- app/src/main/java/org/secuso/privacyfriendlydicegame/dice/DiceFour.java
+++ app/src/main/java/org/secuso/privacyfriendlydicegame/dice/DiceFour.java
@@ -1,3 +1,20 @@
+/*
+ This file is part of Privacy Friendly Dice Game.
+
+ Privacy Friendly PDice Game is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+
+ Privacy Friendly Dice Game is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly Dice Game. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.secuso.privacyfriendlydicegame.dice;
 
 import android.content.Context;
@@ -7,8 +24,9 @@ import android.graphics.Paint;
 import android.view.View;
 
 /**
- * Created by yonjuni on 18.12.15.
+ * @author Karola Marky
  */
+
 public class DiceFour extends View {
 
     Paint paint;
